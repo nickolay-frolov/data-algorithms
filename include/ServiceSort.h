53af4fe45d1c766158804aaf0609423effbe2a00
LORD-dev86 <lord.dev86@gmail.com>
2023-12-28 20:06:34 +0500

    Add Service Sort class

---
+++ include/ServiceSort.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef SERVICESORT_H
+#define SERVICESORT_H
+
+template <class T>
+class ServiceSort
+{
+	public:
+		static void Print(T arr[]);
+		void FillRandomValues(T arr[]);
+};
+
+#endif
