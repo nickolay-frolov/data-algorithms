59a4c4ac4ac07c595bd166447efa4ff4776ce803
LORD-dev86 <lord.dev86@gmail.com>
2024-01-24 19:14:56 +0500

    Add bubble sort and it's improvements

---
+++ include/Sort.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+
+#ifndef SORT_H
+#define SORT_H
+
+template <class T>
+class Sort
+{
+	public:
+	    //Bubble sort and its improvements
+		void Bubble(std::vector<T>& array);
+		void Shaker(std::vector<T>& array);
+		void Comb(std::vector<T>& array);
+
+		//Simple sort
+		//void Insertion(std::vector<T> array);
+		//void Selection(std::vector<T> array);
+		//
+		//Effective sort
+		//void Quick(std::vector<T> array);
+		//void Merge(std::vector<T> array);
+		//void Heap(std::vector<T> array);
+};
+
+#include "..\\src\\Sort.ipp"
+
+#endif
